61d65d0c621f928749da16378b56b61d9a4cafb6
rgmcewan <rgmcewan@outlook.com>
2024-01-30 12:26:11 +1000

    Added click and button down events for +, - and reset buttons. Has bug if you click on increase button and then use keyboard. Bug is fixed later in the course.

--- 02 - Modern JS Fundamentals/project files/fancy counter/script.js
+++ 02 - Modern JS Fundamentals/project files/fancy counter/script.js
@@ -1,3 +1,16 @@
-const increaseButtonEl = document.querySelector('.counter__button counter__button--increase');
+const increaseButtonEl = document.querySelector('.counter__button--increase');
+const counterValueEl = document.querySelector('.counter__value')
 
-console.log(increaseButtonEl);
+increaseButtonEl.addEventListener('click', function() {
+    // get current value of counter
+    const currnetValue = counterValueEl.textContent;
+    
+
+    //convert valye to number type
+    const currnetValueAsNumber = +currnetValue;
+
+    //increment by 1
+
+    //set counter element with new value
+    counterValueEl.textContent = 1;
+})
