a03a2fce823e5e3fd2bd610bda29b83d426f07d0
rgmcewan <rgmcewan@outlook.com>
2024-01-30 12:38:46 +1000

    Replaced duplicate functions with named function.
    Added incrementName function to replate click and buttondown functions.

--- 02 - Modern JS Fundamentals/project files/fancy counter/script.js
+++ 02 - Modern JS Fundamentals/project files/fancy counter/script.js
@@ -1,5 +1,28 @@
 const increaseButtonEl = document.querySelector('.counter__button--increase');
+const decreaseButtonEl = document.querySelector('.counter__button--decrease');
+const resetButtonEl = document.querySelector('.counter__reset-button');
 const counterValueEl = document.querySelector('.counter__value')
+
+resetButtonEl.addEventListener('click', function() {
+    //set counter value to zero
+    counterValueEl.textContent = 0;
+});
+
+
+decreaseButtonEl.addEventListener('click', function() {
+    // get cuttent value of the counter
+    const currentValue = counterValueEl.textContent;
+
+    // convert the value to number type
+    const currnetValueAsNumber = +currentValue;
+
+    // decrement by 1
+    const newValue = currnetValueAsNumber - 1;
+
+    // update counter value with new value
+    counterValueEl.textContent = newValue;
+});
+
 
 increaseButtonEl.addEventListener('click', function() {
     // get current value of counter
@@ -10,7 +33,23 @@ increaseButtonEl.addEventListener('click', function() {
     const currnetValueAsNumber = +currnetValue;
 
     //increment by 1
+    const newValue = currnetValueAsNumber + 1;
 
     //set counter element with new value
-    counterValueEl.textContent = 1;
+    counterValueEl.textContent = newValue;
 })
+
+document.addEventListener('keydown', function() {
+    // get current value of counter
+    const currnetValue = counterValueEl.textContent;
+    
+
+    //convert valye to number type
+    const currnetValueAsNumber = +currnetValue;
+
+    //increment by 1
+    const newValue = currnetValueAsNumber + 1;
+
+    //set counter element with new value
+    counterValueEl.textContent = newValue;  
+});
