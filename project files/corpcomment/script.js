05776bf423c4c4a77e9b65d019831c1650e74288
rgmcewan <rgmcewan@outlook.com>
2024-02-09 12:33:06 +1000

    Working on submitHandler (input validation etc)

--- project files/corpcomment/script.js
+++ project files/corpcomment/script.js
@@ -1,5 +1,9 @@
+// -- GLOBAL --
 const textareaEl = document.querySelector('.form__textarea');
 const counterEl = document.querySelector('.counter');
+const formEl = document.querySelector('.form');
+
+// COUNTER COMPONENT
 
 const inputHandler = () => {
     // determine maximum number of characters
@@ -17,3 +21,30 @@ const inputHandler = () => {
 
 
 textareaEl.addEventListener('input', inputHandler);
+
+
+
+// SUBMIT COMPONENT
+
+const submitHandler = event => {
+    //prevent default browser action (submitting form data to the 'action' address and loading a new page)
+    event.preventDefault();
+
+    // get text from the text area
+    const text = textareaEl.value;
+    
+    // validate test (eg check if hashtag is present and text is long enough)
+    if (text.includes('#')) {
+        formEl.classList.add('form--valid');
+    } else {
+        formEl.classList.add('form--invalid');
+    }
+
+
+    //test the submit and get information about the event in the console (NOT IN COURSE CODE)
+    console.log(text);
+    console.log(1);
+    console.log(event);
+};
+
+formEl.addEventListener('submit', submitHandler);
