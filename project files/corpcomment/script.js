f46ec5ba6715f4d995559cc5009964d06a13540e
rgmcewan <rgmcewan@outlook.com>
2024-02-09 21:06:35 +1000

    Inserted feedback item into list

--- project files/corpcomment/script.js
+++ project files/corpcomment/script.js
@@ -2,6 +2,7 @@
 const textareaEl = document.querySelector('.form__textarea');
 const counterEl = document.querySelector('.counter');
 const formEl = document.querySelector('.form');
+const feedbackListEl = document.querySelector('.feedbacks');
 
 // COUNTER COMPONENT
 
@@ -51,13 +52,49 @@ const submitHandler = event => {
 
     // focus the text area again
     textareaEl.focus();
+    
+    // stop this function executing if the form is invalid
+    return;
 }
 
+    // we have text that is valid, now extract other info from that text
+    const hashtag = text.split(' ').find(word => word.includes('#'));
+    // const hashtag = text.split(' ');
+    // const company;
+    const company = hashtag.substring(1);
+    // const badgeLetter;
+    const badgeLetter = company.substring(0, 1).toUpperCase();
+    const upvoteCount = 0;
+    const daysAgo = 0;
 
+    // new feedback item HTML
+    const feedbackItemHTML = `
+        <li class="feedback">
+            <button class="upvote">
+                <i class="fa-solid fa-caret-up upvote__icon"></i>
+                <span class="upvote__count">${upvoteCount}</span>
+            </button>
+            <section class="feedback__badge">
+                <p class="feedback__letter">${badgeLetter}</p>
+            </section>
+            <div class="feedback__content">
+                <p class="feedback__company">${company}</p>
+                <p class="feedback__text">${text}</p>
+            </div>
+            <p class="feedback__date">${daysAgo}</p>
+        </li>
+    `;
+
+    // Insert new feedback item into list
+    feedbackListEl.insertAdjacentHTML('beforeend', feedbackItemHTML);
+    
+    
     //test the submit and get information about the event in the console (NOT IN COURSE CODE)
     console.log(text);
     console.log(1);
     console.log(event);
+    console.log(company);
+    console.log(badgeLetter);
 };
 
 formEl.addEventListener('submit', submitHandler);
