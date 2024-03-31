9693cc95f55fdac169e487aff09b7115fc118d57
rgmcewan <rgmcewan@outlook.com>
2024-03-31 20:58:14 +1000

    Finished 116 - Storage component (localStorage for  persisting data & spread operator ...)

--- project files/rmtdev/src/components/Bookmarks.js
+++ project files/rmtdev/src/components/Bookmarks.js
@@ -17,6 +17,9 @@ const clickHandler = event => {
         state.bookmarkJobItems.push(state.activeJobItem);
     }
 
+    // persist data with local storage
+    localStorage.setItem('bookmarkJobItems', JSON.stringify(state.bookmarkJobItems));
+
 
     // update bookmark icon
     document.querySelector('.job-info__bookmark-icon').classList.toggle('job-info__bookmark-icon--bookmarked');
